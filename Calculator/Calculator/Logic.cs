d01248c824a05fbf32fa6c4835b24e3d5e0988e3
Erice Dela Cruz <104292436+xtra-rice@users.noreply.github.com>
2024-01-05 10:42:01 +0800

    Add files via upload

---
+++ Calculator/Calculator/Logic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Logic
+    {
+        public void Add(float n1, float n2)
+        { 
+
+            float result = n1 + n2;
+            Console.WriteLine($"{n1} + {n2} = {result}\n");
+        }
+            
+        public void Subtract(float n1, float n2)
+        {
+            float result = n1 - n2;
+            Console.WriteLine($"{n1} - {n2} = {result}\n");
+        }
+
+        public void Multiply(float n1, float n2)
+        {
+            float result = n1 * n2;
+            Console.WriteLine($"{n1} * {n2} = {result}\n");
+        }
+
+        public void Divide(float n1, float n2)
+        {
+            if(n2 != 0)
+            {
+                float result = n1 / n2;
+                float roundOff = (float)Math.Round(result, 2);
+                Console.WriteLine($"{n1} / {n2} = {roundOff}\n");
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by 0\n");
+            }
+           
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("|             CALCULATOR             |");
+            Console.WriteLine("-------------------------------------");
+
+            Logic myObj = new Logic();
+
+
+            while (true)
+            {
+                Console.Write("Enter operation: " +
+                    "1.Addition " +
+                    "2.Subtraction " +
+                    "3.Multiplication " +
+                    "4.Division " +
+                    "5.Quit: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int operation) || operation < 1 || operation > 5)
+                {
+                    Console.Write("Invalid operation. Please enter a valid option (1-5). \n\n");
+                    continue;
+                }
+
+                if (operation == 5)
+                {
+                    Environment.Exit(0);
+                }
+
+
+                while (true)
+                {
+                    Console.Write("Enter the 1st number: ");
+                    if (!float.TryParse(Console.ReadLine(), out float n1))
+                    {
+                        Console.WriteLine("Invalid number. Please enter a valid number.\n");
+                        continue;
+                    }
+
+                    Console.Write("Enter the 2nd number: ");
+                    if (!float.TryParse(Console.ReadLine(), out float n2))
+                    {
+                        Console.WriteLine("Invalid number. Please enter a valid number.\n");
+                        continue;
+                    }
+
+
+                    switch (operation)
+                    {
+                        case 1:
+                            myObj.Add(n1, n2);
+                            break;
+
+                        case 2:
+                            myObj.Subtract(n1, n2);
+                            break;
+
+                        case 3:
+                            myObj.Multiply(n1, n2);
+                            break;
+
+                        case 4:
+                            myObj.Divide(n1, n2);
+                            break;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
